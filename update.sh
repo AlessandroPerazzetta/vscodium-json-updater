8bac4ae8e641d86d1c183c758a336f6f5864089c
AlessandroPerazzetta <alessandro.perazzetta@gmail.com>
2021-05-17 13:37:26 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NOW=$(date +"%Y%m%d-%H%M")
+SRC_DIR='/usr/share/codium/resources/app/'
+JSON_FILE='product.json'
+SRC_FILE=$SRC_DIR$JSON_FILE
+BKP_FILE=$SRC_DIR$JSON_FILE.$NOW
+
+cp $SRC_FILE $BKP_FILE
+jq '.extensionsGallery' $BKP_FILE
+jq 'del(.extensionsGallery[])' $BKP_FILE | jq '.extensionsGallery |= . + {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"}' > $SRC_FILE
+jq '.extensionsGallery ' $SRC_FILE
