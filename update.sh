63b27b9de564c2f380b99985097a99af0cd527a9
Alessandro Perazzetta <alessandro.perazzetta@gmail.com>
2021-07-07 14:08:07 +0200

    add check if jq exist, add prefix with sudo

--- update.sh
+++ update.sh
@@ -5,7 +5,11 @@ JSON_FILE='product.json'
 SRC_FILE=$SRC_DIR$JSON_FILE
 BKP_FILE=$SRC_DIR$JSON_FILE.$NOW
 
-cp $SRC_FILE $BKP_FILE
-jq '.extensionsGallery' $BKP_FILE
-jq 'del(.extensionsGallery[])' $BKP_FILE | jq '.extensionsGallery |= . + {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"}' > $SRC_FILE
-jq '.extensionsGallery ' $SRC_FILE
+if ! [ -x "$(command -v jq)" ]; then
+	echo 'Error: jq is not installed.' >&2
+	exit 1
+fi
+sudo cp $SRC_FILE $BKP_FILE
+sudo jq '.extensionsGallery' $BKP_FILE
+sudo jq 'del(.extensionsGallery[])' $BKP_FILE | jq '.extensionsGallery |= . + {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"}' > $SRC_FILE
+sudo jq '.extensionsGallery ' $SRC_FILE
