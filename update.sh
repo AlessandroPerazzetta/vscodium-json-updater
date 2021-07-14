29d8ec4fd8a7e87d247917bf9cf1d84ecef74e1b
AlessandroPerazzetta <alessandro.perazzetta@gmail.com>
2021-07-14 14:53:06 +0200

    Change sudo multi command

--- update.sh
+++ update.sh
@@ -9,7 +9,9 @@ if ! [ -x "$(command -v jq)" ]; then
 	echo 'Error: jq is not installed.' >&2
 	exit 1
 fi
-sudo cp $SRC_FILE $BKP_FILE
-sudo jq '.extensionsGallery' $BKP_FILE
-sudo jq 'del(.extensionsGallery[])' $BKP_FILE | sudo jq '.extensionsGallery |= . + {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"}' > $SRC_FILE
-sudo jq '.extensionsGallery ' $SRC_FILE
+sudo -i -- <<EOF
+cp $SRC_FILE $BKP_FILE
+jq '.extensionsGallery' $BKP_FILE
+jq 'del(.extensionsGallery[])' $BKP_FILE | jq '.extensionsGallery |= . + {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"}' > $SRC_FILE
+jq '.extensionsGallery ' $SRC_FILE
+EOF
